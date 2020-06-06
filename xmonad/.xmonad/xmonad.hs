11d1affad497907dcc96991e5ba75e1c8d42352e
Rose <rose@empty.town>
2020-06-06 16:59:44 +0100

    Sat  6 Jun 16:59:44 BST 2020

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,43 +1,53 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase    #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
-import           XMonad
-import           XMonad.Hooks.DynamicBars
-import           XMonad.Hooks.DynamicLog
-import           XMonad.Hooks.ManageDocks
-import           XMonad.Layout.Grid
-import           XMonad.Layout.LayoutModifier
-import           XMonad.Layout.Spacing
 import Graphics.X11.ExtraTypes.XF86
+import XMonad
+import XMonad.Hooks.DynamicBars
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Grid
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.Spacing
+import Colors
 
 import qualified Data.Map as M
 
 featureColor :: String
-featureColor = "#FC4E71"
+featureColor = color2
 
 urgentColor :: String
-urgentColor = "#ef3e36"
+urgentColor = color5
 
 dimColor :: String
-dimColor = "#777"
+dimColor = color8
 
 dimmerColor :: String
-dimmerColor = "#444"
+dimmerColor = color9
 
 gapSize :: Integer
 gapSize = 5
 
 main :: IO ()
 main = xmonad =<< statusBar "xmobar" myPP (const (mod1Mask, xK_b)) def
-  { terminal = "termite"
+  { terminal = "urxvt"
   , modMask = mod4Mask
   , borderWidth = 4
   , focusedBorderColor = featureColor
-  , workspaces = ["☰", "☱", "☲", "☳", "☴", "☵", "☶", "☷"]
+  , workspaces = ["i", "ii", "iii", "iv"]
   , layoutHook = avoidStruts
-      ( tall ||| Full ||| gaps tall ||| gaps grid )
+      ( tall ||| Full ||| gaps tall ||| gaps (Mirror tall) ||| gaps grid )
   , keys = myKeymap
   }
+
+dmenu :: String
+dmenu = mconcat
+  [ "dmenu_run -fn \"Misc Terminusmodx:size=9\""
+  , " -sb " ++ mconcat ["\"", featureColor, "\""]
+  , " -nb " ++ mconcat ["\"", background, "\""]
+  , " -nf " ++ mconcat ["\"", foreground, "\""]
+  , " -sf " ++ mconcat ["\"", background, "\""]
+  ]
 
 myKeymap :: XConfig Layout -> M.Map (ButtonMask, KeySym) (X ())
 myKeymap layout = M.fromList keybinds <> keys def layout
@@ -45,7 +55,7 @@ myKeymap layout = M.fromList keybinds <> keys def layout
           [
             ((mod4Mask .|. shiftMask, xK_s), spawn "shootregion")
           , ((mod4Mask .|. shiftMask, xK_e), spawn "emacsclient --create-frame")
-          , ((mod4Mask, xK_p), spawn $ "dmenu_run -fn CozetteVector:size=9 -sb " ++ mconcat ["\"", featureColor, "\""])
+          , ((mod4Mask, xK_p), spawn dmenu)
           , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10")
           , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
           ]
@@ -65,15 +75,16 @@ grid = GridRatio (3/3)
 
 myPP :: PP
 myPP = def
-  { ppCurrent = xmobarColor featureColor ""
-  , ppUrgent = xmobarColor urgentColor ""
+  { ppCurrent = const $ xmobarColor featureColor "" "\x00e5"
+  , ppUrgent = const $ xmobarColor urgentColor "" "\x00e5"
   , ppLayout = \case
-      "Tall"          -> "[│]"
-      "Full"          -> "[ ]"
-      "Spacing Tall"  -> "[║]"
-      "Spacing GridRatio 1.0" -> "[#]"
-  , ppSep = xmobarColor dimColor "" " ∙ "
+      "Tall"          -> "\x00fe"
+      "Full"          -> "\x00ff"
+      "Spacing Tall"  -> "\x00f6"
+      "Spacing Mirror Tall" -> "\x00fc"
+      "Spacing GridRatio 1.0" -> "\x00fa"
+  , ppSep = xmobarColor dimColor "" " / "
   , ppTitle = xmobarColor dimColor ""
-  , ppHidden = id
-  , ppHiddenNoWindows = xmobarColor dimColor ""
+  , ppHidden = const $ "\x00e5"
+  , ppHiddenNoWindows = const $ xmobarColor dimColor "" "\x00e5"
   }
